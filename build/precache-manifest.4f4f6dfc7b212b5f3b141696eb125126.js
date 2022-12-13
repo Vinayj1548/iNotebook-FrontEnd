5cb5af7b755b85d3d233a8a53f35817ac4a70287
Vinayj1548 <113287840+Vinayj1548@users.noreply.github.com>
2022-12-13 13:46:11 +0530

    Add files via upload

---
+++ build/precache-manifest.4f4f6dfc7b212b5f3b141696eb125126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dda112924e27b01abb6",
+    "url": "/iNotebook-React/static/css/main.eae9b873.chunk.css"
+  },
+  {
+    "revision": "5dda112924e27b01abb6",
+    "url": "/iNotebook-React/static/js/main.5dda1129.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/iNotebook-React/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "cb2bd676ad692ed768e5",
+    "url": "/iNotebook-React/static/js/2.cb2bd676.chunk.js"
+  },
+  {
+    "revision": "0decb955950cb2546d8f",
+    "url": "/iNotebook-React/static/js/runtime~main.0decb955.js"
+  },
+  {
+    "revision": "b4e4879bf4d0fd87011c4f581a243b73",
+    "url": "/iNotebook-React/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import NoteState from "./context/notes/NoteState";
+
+function App() {
+  const [alert , setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home showAlert={showAlert} />} />
+              <Route path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+              <Route path="/signup" element={<Signup showAlert={showAlert} />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+};
+
+export default App;
